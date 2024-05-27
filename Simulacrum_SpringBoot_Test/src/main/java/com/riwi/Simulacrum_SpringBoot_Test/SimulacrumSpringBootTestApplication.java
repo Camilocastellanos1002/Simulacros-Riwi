401cbe8b087c8bd8a4c94923ac896aaf29aeeb3c
Juan Camilo <jccaste1002@gmail.com>
2024-05-27 08:41:03 -0500

    Simulacrum SpringBoot Test

---
+++ Simulacrum_SpringBoot_Test/src/main/java/com/riwi/Simulacrum_SpringBoot_Test/SimulacrumSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.Simulacrum_SpringBoot_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacrumSpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacrumSpringBootTestApplication.class, args);
+	}
+
+}
---
+++ Simulacrum_SpringBoot_Test/src/test/java/com/riwi/Simulacrum_SpringBoot_Test/SimulacrumSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.Simulacrum_SpringBoot_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacrumSpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
